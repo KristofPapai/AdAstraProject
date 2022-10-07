d51846cbb652018bdc67a5542a5dd5a84a9dfec8
Kristóf Levente Pápai <kristofpapai@gmail.com>
2022-10-07 15:40:48 +0200

    layered noise on planet mesh

--- AdAstraTestProject/Assets/Scripts/PorceduralPlanet/ShapeGenerator.cs
+++ AdAstraTestProject/Assets/Scripts/PorceduralPlanet/ShapeGenerator.cs
@@ -5,15 +5,39 @@ using UnityEngine;
 public class ShapeGenerator
 {
     ShapeSettings settings;
+    NoiseFilter[] noiseFilter;
 
     public ShapeGenerator(ShapeSettings settings)
     {
         this.settings = settings;
+        noiseFilter = new NoiseFilter[settings.noiseLayers.Length];
+        for (int i = 0; i < noiseFilter.Length; i++)
+        {
+            noiseFilter[i] = new NoiseFilter(settings.noiseLayers[i].noiseSettings);
+        }
     }
 
     public Vector3 CalculatePointOnPlanet(Vector3 pointOnUnitSphere)
     {
-        return pointOnUnitSphere * settings.planetRadius;
+        float firstLayerValue = 0;
+        float elevation = 0;
+        if (noiseFilter.Length > 0)
+        {
+            firstLayerValue = noiseFilter[0].Evaluate(pointOnUnitSphere);
+            if (settings.noiseLayers[0].enabled)
+            {
+                elevation = firstLayerValue;
+            }
+        }
+        for (int i = 0; i < noiseFilter.Length; i++)
+        {
+            if (settings.noiseLayers[i].enabled)
+            {
+                float mask = settings.noiseLayers[i].useFirstLayerAsMask?firstLayerValue:1;
+                elevation += noiseFilter[i].Evaluate(pointOnUnitSphere)*mask;
+            }
+        }
+        return pointOnUnitSphere * settings.planetRadius* (1 + elevation);
     }
 
 }

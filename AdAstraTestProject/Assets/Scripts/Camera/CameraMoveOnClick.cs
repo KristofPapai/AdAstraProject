751bbc55863b2d9e48c6c916ac4613bc50e746ae
Kristóf Levente Pápai <kristofpapai@gmail.com>
2022-11-01 17:08:02 +0100

    camera debug with GUI elements

--- AdAstraTestProject/Assets/Scripts/Camera/CameraMoveOnClick.cs
+++ AdAstraTestProject/Assets/Scripts/Camera/CameraMoveOnClick.cs
@@ -1,8 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+//using System.Numerics;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
+
 
 public class CameraMoveOnClick : MonoBehaviour
 {
@@ -13,9 +15,134 @@ public class CameraMoveOnClick : MonoBehaviour
     private bool moveToCelestial = false;
     private bool chaseCam = false;
     private bool reset = false;
+
+
+    private bool camResetMode = false;
+    private bool GUIControl = false;
+
+    private Vector3 scrollViewVector = Vector3.zero;
+    public Rect dropDownRect = new Rect(125, 50, 125, 300);
+   
+
+    int indexNumber;
+    bool show = false;
+
+
+    private void OnGUI()
+    {
+        GameObject[] planets = GameObject.FindGameObjectsWithTag("Celestial");
+        if (GUI.Button(new Rect((dropDownRect.x - 100), dropDownRect.y, dropDownRect.width, 25), ""))
+        {
+            if (!show)
+            {
+                show = true;
+            }
+            else
+            {
+                show = false;
+            }
+        }
+        if (show)
+        {
+            scrollViewVector = GUI.BeginScrollView(new Rect((dropDownRect.x - 100), (dropDownRect.y + 25), dropDownRect.width, dropDownRect.height), scrollViewVector, new Rect(0, 0, dropDownRect.width, Mathf.Max(dropDownRect.height, (planets.Length * 25))));
+            GUI.Box(new Rect(0, 0, dropDownRect.width, Mathf.Max(dropDownRect.height, (planets.Length * 25))), "");
+            for (int index = 0; index < planets.Length; index++)
+            {
+                if (GUI.Button(new Rect(0, (index * 25), dropDownRect.height, 25), ""))
+                {
+                    show = false;
+                    indexNumber = index;
+                }
+                GUI.Label(new Rect(5, (index * 25), dropDownRect.height, 25), planets[index].name);
+                //CameraControll(planets[index].name);
+            }
+            GUI.EndScrollView();
+        }
+        else
+        {
+            GUI.Label(new Rect((dropDownRect.x - 95), dropDownRect.y, 300, 25), planets[indexNumber].name);
+            Debug.Log(planets[indexNumber].name);
+
+            if (GUIControl)
+            {
+                selected = GameObject.Find(planets[indexNumber].name);
+
+                float distance = Vector3.Distance(mainCamera.transform.position, new Vector3(selected.transform.position.x, selected.transform.position.y + 40, selected.transform.position.z - 40));
+                if (camResetMode == false)
+                {
+                    //selected = GameObject.Find(planets[indexNumber].name);
+                    var step = speed * Time.deltaTime;
+                    mainCamera.transform.position = Vector3.MoveTowards(mainCamera.transform.position, new Vector3(selected.transform.position.x, selected.transform.position.y + 40, selected.transform.position.z - 40), step);
+                    distance = Vector3.Distance(mainCamera.transform.position, new Vector3(selected.transform.position.x, selected.transform.position.y + 40, selected.transform.position.z - 40));
+
+                }
+                if (distance < 10)
+                {
+                    camResetMode = true;
+                    mainCamera.transform.position = new Vector3(selected.transform.position.x, selected.transform.position.y + 40, selected.transform.position.z - 40);
+                }
+            }
+        }
+    }
+
+    public void CameraReset()
+    {
+        mainCamera.transform.position = cameraStartPos;
+    }
     
-    
-    
+    public void CameraControll()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            camResetMode = false;
+            GUIControl = false;
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.SphereCast(ray,30, out hit, 1000f) && !EventSystem.current.IsPointerOverGameObject())
+            {
+                if (hit.transform.tag == "Celestial" && selected != hit.transform.gameObject && hit.transform.gameObject.layer != 4)
+                {
+                    selected = hit.transform.gameObject;
+                    Debug.Log("Celestial Hit");
+                    moveToCelestial = true;
+                    chaseCam = false;
+                }
+            }
+        }
+        if (moveToCelestial)
+        {
+            var targetRotation = Quaternion.LookRotation(selected.transform.position - transform.position);
+            var step = speed * Time.deltaTime;
+            float distance = Vector3.Distance(mainCamera.transform.position, new Vector3(selected.transform.position.x, selected.transform.position.y + 40, selected.transform.position.z - 40));
+            if (distance != 0)
+            {
+                mainCamera.transform.position = Vector3.MoveTowards(mainCamera.transform.position, new Vector3(selected.transform.position.x, selected.transform.position.y + 40, selected.transform.position.z - 40), step);
+                Debug.Log("shake");
+            }
+            else
+            {
+                chaseCam = true;
+
+            }
+            if (chaseCam)
+            {
+                mainCamera.transform.position = new Vector3(selected.transform.position.x, selected.transform.position.y + 40, selected.transform.position.z - 40);
+            }
+        }
+        else
+        {
+            GUIControl = true;
+        }
+        if (Input.GetKey("r"))
+        {
+            CameraReset();
+            moveToCelestial = false;
+            camResetMode = true;
+            GUIControl = true;
+        }
+
+
+    }
 
     void Start()
     {
@@ -26,61 +153,8 @@ public class CameraMoveOnClick : MonoBehaviour
 
     void Update()
     {
-       
-        if (Input.GetMouseButtonDown(0))
-        {
-            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            if (Physics.Raycast(ray, out hit, 1000f) && !EventSystem.current.IsPointerOverGameObject())
-            {
-                if (hit.transform.tag == "Celestial" && selected != hit.transform.gameObject && hit.transform.gameObject.layer != 4)
-                {
-                    selected = hit.transform.gameObject;
-                    Debug.Log("Celestial Hit");
-                    moveToCelestial=true;
-                    chaseCam = false;
-                }
-            }
-        }
-        if (moveToCelestial)
-        {
-            var targetRotation = Quaternion.LookRotation(selected.transform.position - transform.position);
-            var step = speed * Time.deltaTime;
-            float distance = Vector3.Distance(mainCamera.transform.position, new Vector3(selected.transform.position.x, selected.transform.position.y+40, selected.transform.position.z-40));
-            if (distance != 0)
-            {
-                mainCamera.transform.position = Vector3.MoveTowards(mainCamera.transform.position, new Vector3(selected.transform.position.x, selected.transform.position.y + 40, selected.transform.position.z - 40), step);
-                //mainCamera.transform.LookAt(selected.transform);
-                //transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, speed * Time.deltaTime);
-                Debug.Log("shake");
-            }
-            else
-            {
-                chaseCam = true;
-                
-            }
-            if (chaseCam)
-            {
-                mainCamera.transform.position = new Vector3(selected.transform.position.x, selected.transform.position.y + 40, selected.transform.position.z-40);
-                //mainCamera.transform.LookAt(selected.transform);
-                //transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, speed * Time.deltaTime);
-            }
-        }
-
-        if (Input.GetKey("r"))
-        {
-            reset = true;
-        }
-        if (reset)
-        {
-            moveToCelestial = false;
-            var step = speed * Time.deltaTime;
-            mainCamera.transform.position = Vector3.MoveTowards(mainCamera.transform.position, cameraStartPos, step);
-        }
-        if (reset && mainCamera.transform.position == cameraStartPos)
-        {
-            reset = false;
-        }
-
+        CameraControll();
     }
+
+
 }
--- AdAstraTestProject/Assets/Scripts/OrbitMaster.cs
+++ AdAstraTestProject/Assets/Scripts/OrbitMaster.cs
@@ -16,6 +16,8 @@ public class OrbitMaster : MonoBehaviour
     public float rotationSpeed;
     public float dampAnt;
     public Button hudbutton;
+    private List<GameObject> planets = new List<GameObject>();
+    public Camera mainCamera;
     //public Transform parent;
 
     // Start is called before the first frame update
@@ -32,7 +34,38 @@ public class OrbitMaster : MonoBehaviour
         float maxOrbitRing = numberOfPlanets * 80;
         float dampningUpdate = 6;
         string[] solType = new string[] { "WD", "WR", "RG", "BD", "NE" };
+        string[] latinNames = new string[] { "Yolninus",
+                                            "Sophoria",
+                                            "Lucronoe",
+                                            "Dulides",
+                                            "Choanope",
+                                            "Ilea",
+                                            "Llezistea",
+                                            "Sulutov",
+                                            "Made A75",
+                                            "Llarth W",
+                                            "Komehines",
+                                            "Tholnenides",
+                                            "Sungade",
+                                            "Ulvillon",
+                                            "Geutania",
+                                            "Vavis",
+                                            "Namaria",
+                                            "Vineturn",
+                                            "Garth U7G3",
+                                            "Leron 7F9",
+                                            "Licrenope",
+                                            "Mecraeturn",
+                                            "Gilichi",
+                                            "Cuzarvis",
+                                            "Piohines",
+                                            "Iarus",
+                                            "Grebicarro",
+                                            "Zutheter",
+                                            "Trypso 12RZ",
+                                            "Neron I50"};
         string inSol = solType[Random.Range(0, solType.Length)];
+        List<string> choosenNames = new List<string>();
         for (int i = 0; i < numberOfPlanets; i++)
         {
 
@@ -46,9 +79,19 @@ public class OrbitMaster : MonoBehaviour
             dampningUpdate -= 0.5f;
             var childPlanet = Instantiate(prefab, new Vector3(newX, 0, 0), Quaternion.identity);
 
+            bool goodName = false;
+            do
+            {
+                string name = latinNames[Random.Range(0, latinNames.Length)];
+                if (!choosenNames.Contains(name))
+                {
+                    choosenNames.Add(name);
+                    childPlanet.name = name;
+                    goodName = true;
+                }
 
-            childPlanet.name = planetNameGenerator(inSol);
-
+            } while (goodName != true);
+            
             int typeOfPlanet = Random.Range(0, 4);
             float sizeOfPlanet;
             //ha 0-nál több holdat akar generálni akkor majd figyelni kell a oldak távolságára
@@ -80,17 +123,92 @@ public class OrbitMaster : MonoBehaviour
             childPlanet.transform.parent = parentPlanet.transform;
             parentPlanet.transform.rotation = Quaternion.Euler(0, 0, orbitrotation);
             parentPlanet.name = "parent " + childPlanet.name;
+            planets.Add(childPlanet);
 
         }
     }
 
-    public string planetNameGenerator(string solType)
-    {
-        //string[] solType = new string[] { "WD", "WR", "RG", "BD", "NE" };
-        int starCord = Random.Range(1,int.MaxValue);
-        string[] potential = new string[] { "G", "M", "P", "B" };
-        return solType + " " + starCord + potential[Random.Range(0, potential.Length)];
 
-    }
+    private Vector2 scrollViewVector = Vector2.zero;
+    public Rect dropDownRect = new Rect(125, 50, 125, 300);
+    public static string[] list = { "List of Planets in the system" };
 
+    int indexNumber;
+    bool show = false;
+
+    //private void OnGUI()
+    //{
+    //    if (GUI.Button(new Rect((dropDownRect.x - 100), dropDownRect.y, dropDownRect.width, 25), ""))
+    //    {
+    //        if (!show)
+    //        {
+    //            show = true;
+    //        }
+    //        else
+    //        {
+    //            show = false;
+    //        }
+    //    }
+    //    if (show)
+    //    {
+    //        scrollViewVector = GUI.BeginScrollView(new Rect((dropDownRect.x - 100), (dropDownRect.y + 25), dropDownRect.width, dropDownRect.height), scrollViewVector, new Rect(0, 0, dropDownRect.width, Mathf.Max(dropDownRect.height, (list.Length * 25))));
+
+    //        GUI.Box(new Rect(0, 0, dropDownRect.width, Mathf.Max(dropDownRect.height, (planets.Count * 25))), "");
+
+    //        for (int index = 0; index < planets.Count; index++)
+    //        {
+
+    //            if (GUI.Button(new Rect(0, (index * 25), dropDownRect.height, 25), ""))
+    //            {
+    //                show = false;
+    //                indexNumber = index;
+    //            }
+
+    //            GUI.Label(new Rect(5, (index * 25), dropDownRect.height, 25), planets[index].name);
+
+    //        }
+
+            
+
+    //        GUI.EndScrollView();
+    //    }
+    //    else
+    //    {
+
+    //        GUI.Label(new Rect((dropDownRect.x - 95), dropDownRect.y, 300, 25), planets[indexNumber].name);
+
+    //        CamMoveToListTarget(planets[indexNumber].name);
+    //    }
+    //}
+
+    //private bool chaseCam = false;
+    //public float speed = 100.0f;
+    //public void CamMoveToListTarget(string planetName)
+    //{
+
+    //    GameObject selected = GameObject.Find(planetName);
+
+    //    var step = speed * Time.deltaTime;
+    //    float distance = Vector3.Distance(mainCamera.transform.position, new Vector3(selected.transform.position.x, selected.transform.position.y + 40, selected.transform.position.z - 40));
+    //    if (distance != 0)
+    //    {
+    //        mainCamera.transform.position = Vector3.MoveTowards(mainCamera.transform.position, new Vector3(selected.transform.position.x, selected.transform.position.y + 40, selected.transform.position.z - 40), step);
+    //        //mainCamera.transform.LookAt(selected.transform);
+    //        //transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, speed * Time.deltaTime);
+    //        Debug.Log("shake");
+            
+    //    }
+    //    else
+    //    {
+    //        chaseCam = true;
+
+    //    }
+    //    if (chaseCam)
+    //    {
+    //        mainCamera.transform.position = new Vector3(selected.transform.position.x, selected.transform.position.y + 40, selected.transform.position.z - 40);
+    //        //mainCamera.transform.LookAt(selected.transform);
+    //        //transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, speed * Time.deltaTime);
+    //    }
+
+    //}
 }
